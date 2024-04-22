055a8107e44d9f5046a48bb0a2ac5748d2946217
Marcomagnano93 <marcog.magnano@gmail.com>
2024-04-22 12:17:40 +0200

    FIRST COMMIT: add vite, vue & sass

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+  import AppHeader from "./components/AppHeader.vue";
+
+  export default{
+    components: {
+      AppHeader,
+    }
+  }
+
+</script>
+
+<template>
+  <header>
+    <AppHeader/>
+
+  </header>
+
+  <main>
+
+  </main>
+
+  <footer>
+
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
